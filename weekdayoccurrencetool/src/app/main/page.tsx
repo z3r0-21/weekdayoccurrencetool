4f9a953c2fe46dfb84c027f266200e1320e0ec05
Dimitar Prisadnikov <mitko1pr@gmail.com>
2023-10-12 13:01:26 +0200

    basic project structure

---
+++ weekdayoccurrencetool/src/app/main/page.tsx
@@ -0,0 +1,11 @@
+import InputComponent from "@/app/main/input-component/input";
+import GraphComponent from "@/app/main/graph-component/graph";
+
+export default function Main() {
+  return (
+    <>
+      <InputComponent />
+      <GraphComponent />
+    </>
+  );
+}
